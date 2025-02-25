f60ea4d404290625927c5bda764147132a152f0e
Hermann-Jude Rencontre <93084483+HJRencontre@users.noreply.github.com>
2025-02-25 19:08:11 +0100

    Revert "feat(SPO-24): install shadcn component library"

--- front/vite.config.ts
+++ front/vite.config.ts
@@ -1,39 +1,16 @@
-import tailwindcss from '@tailwindcss/vite';
-import react from '@vitejs/plugin-react';
-import { fileURLToPath } from 'node:url';
-import { defineConfig } from 'vite';
+import {defineConfig} from "vite";
+
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-    base: '/',
-    resolve: {
-        alias: getAliases(),
-    },
-    plugins: [react(), tailwindcss()],
-    preview: {
-        port: 5173,
-        strictPort: true,
-    },
-    server: {
-        port: 5173,
-        strictPort: true,
-        host: true,
-        origin: 'http://0.0.0.0:5173',
-    },
-});
-
-function getAliases() {
-    return [
-        {
-            find: '@',
-            replacement: fileURLToPath(new URL('./src', import.meta.url)),
-        },
-        {
-            find: '@components',
-            replacement: fileURLToPath(new URL('./src/components', import.meta.url)),
-        },
-        {
-            find: '@styles',
-            replacement: fileURLToPath(new URL('./src/styles', import.meta.url)),
-        },
-    ];
-}
+  base: "/",
+  plugins: [react()],
+  preview: {
+    port: 5173,
+    strictPort: true,
+  }, server: {
+    port: 5173,
+    strictPort: true,
+    host: true,
+    origin: "http://0.0.0.0:5173",
+  },});
